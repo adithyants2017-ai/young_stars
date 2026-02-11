e7baa6040d5fbf4151abf1b9eae67b23986d51db
adithyan <adithyants2017@gmail.com>
2026-02-12 02:05:37 +0530

    Prepare for Render deployment with ALLOWED_HOSTS update

--- club_project/settings.py
+++ club_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1 localhost .vercel.app').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1 localhost .vercel.app .onrender.com').split(' ')
 
 
 # Application definition
